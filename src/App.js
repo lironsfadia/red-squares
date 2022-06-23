5382b4d354407b20da8550afd31cd3d0b7cefa1c
Liron <liron.br@bw-robotics.com>
2022-06-23 13:14:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+function doubleMe(){
+  const node = document.createElement("div"); 
+
+  node.setAttribute("id", "header1"); 
+
+  node.style.cssText = 'border: solid 1px red;width: 200px;height: 200px;'; 
+
+  document.getElementById("header").appendChild(node); 
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="header" className="red box" onClick={doubleMe}>
     </div>
   );
 }
